b19ae1f2f10abb703bf352cd141e668b071e7e14
Geethanjali Charvakula <geethanjali.c@svew.edu.in>
2021-11-08 02:01:37 -0800

    change states

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Message from './components/Message';
  
 
 function App() {
   return (
     <div className="App">
-    <Greet name = "geethanjali"  college = "IIT">
+    <Message/>
+
+    <Greet name = "virat"  college = "IIT">
       <p> this is children property</p>
     </Greet>
-    <Greet name = "charvakula" college = "Oxford"/>
-    
 
-    <Welcome/>
+    <Greet name = "kohli" college = "Oxford">
+      <button>click me</button>
+    </Greet>
+    <Greet />
+
+    <Welcome name ="virat" alias = "kohli"/>
 
     </div>
   );
