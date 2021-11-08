2ec094b521cdcfd8d02bf733806e264dff4ffff9
Geethanjali Charvakula <geethanjali.c@svew.edu.in>
2021-11-08 02:29:15 -0800

    counter increment button click

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
+import Counter from './components/Counter';
+ 
  
 
 function App() {
   return (
     <div className="App">
+       <Counter/>
     <Message/>
-
+    
     <Greet name = "virat"  college = "IIT">
       <p> this is children property</p>
     </Greet>
