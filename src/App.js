259c9e1d8cb6002470a1a54eae45abf00e1e80bf
Geethanjali Charvakula <geethanjali.c@svew.edu.in>
2021-11-09 02:01:28 -0800

    method as props

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
 import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
+ 
+
 
 
 function App() {
@@ -15,6 +18,7 @@ function App() {
        <ClassClick/>
     <FunctionClick/>
        <Counter/>
+       <ParentComponent/>
     <Message/>
     
     <Greet name = "virat"  college = "IIT">
