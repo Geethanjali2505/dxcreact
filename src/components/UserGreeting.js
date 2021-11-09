ddde2247b359e5cadd45878102754c407f77dfb8
Geethanjali Charvakula <geethanjali.c@svew.edu.in>
2021-11-09 02:46:40 -0800

    conditional rendering using short circuit operator

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -10,12 +10,28 @@ export class UserGreeting extends Component {
     }
     
     render() {
+
+       return this.state.isLoggedIn && <div>welcome anjali</div>
+       /*  return(
+            this.state.isLoggedIn ?
+            <div>welcome anjali</div> :
+            <div>welcome guest</div>
+        ) */
+
+       /*  let message 
         if(this.state.isLoggedIn){
+            message = <div>welcome anjali</div>
+        }
+        else{
+            message = <div>welcome guest</div>
+        }
+        return <div>{message}</div> */
+       /*  if(this.state.isLoggedIn){
             return <div>welcome anjali</div>
         }
         else{
             return <div>welcome guest</div>
-        }
+        } */
        /*  return (
             <div>
                 <div>welcome anjali</div>
