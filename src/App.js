dbe0f80b5e2939c42be539a6fae220a592cfffb3
Geethanjali Charvakula <geethanjali.c@svew.edu.in>
2021-11-08 22:43:02 -0800

    event binding

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Message from './components/Message';
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
- 
+import EventBind from './components/EventBind';
+
 
 function App() {
   return (
@@ -26,7 +27,7 @@ function App() {
     <Greet />
 
     <Welcome name ="virat" alias = "kohli"/>
-
+    <EventBind/>
     </div>
   );
 }
