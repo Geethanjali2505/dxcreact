64b44620724236f3ba1ba6cd62719152349f1deb
Geethanjali Charvakula <geethanjali.c@svew.edu.in>
2021-11-09 22:13:20 -0800

    styling.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
@@ -7,14 +6,14 @@ import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
 import EventBind from './components/EventBind';
+import NameList from './components/NameList';
 import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
- 
-
+import Stylesheet from './components/Stylesheet';
 function App() {
   return (
     <div className="App">
+      <Stylesheet/>
        <NameList/>
        <ClassClick/>
     <FunctionClick/>
