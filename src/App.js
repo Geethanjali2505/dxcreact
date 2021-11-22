5e82964ba6e2a869d74d0e0f93066798d04ee633
Geethanjali Charvakula <geethanjali.c@svew.edu.in>
2021-11-21 21:40:44 -0800

    passing additional value to action field

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import CounterOne from './components/reducerhook/CounterOne';
+import CounterTwo from './components/reducerhook/CounterTwo';
 
 
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
      <CounterOne/>
+     <CounterTwo/>
     </div>
   );
 }
